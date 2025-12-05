fbc1bd795d788f6dd9495a823b5c569d74107606
Uddipta <uddiptagogoi2000@gmail.com>
2025-12-05 22:42:05 +0530

    Add frontend and payload contents to monorepo

---
+++ payload/src/collections/Posts.ts
@@ -0,0 +1,22 @@
+import type { CollectionConfig } from 'payload'
+
+export const Posts: CollectionConfig = {
+  slug: 'posts',
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'content',
+      type: 'richText',
+    },
+    {
+      name: 'publishedAt',
+      type: 'date',
+    },
+  ],
+}
+
+export default Posts
